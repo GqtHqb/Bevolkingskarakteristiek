9893804edfac47b21c6c011682ecb680ba8c87ed
GqtHqb <58915591+GqtHqb@users.noreply.github.com>
2025-03-01 00:47:59 +0100

    Update Bevolkingskarakteristiek.py

--- Bevolkingskarakteristiek.py
+++ Bevolkingskarakteristiek.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cbsodata
 import pandas as pd
 import re
+import numpy as np
 
 pd.set_option('display.max_rows', 100)
 
@@ -9,18 +10,11 @@ pd.set_option('display.max_rows', 100)
 
 class CBSData:
     def __init__(self, jaar=2024):
-        # self.table_column_ids = {
-        #     2024: {'table_id': '85984NED'},
-        #     2023: {'table_id': '85618NED'},
-        #     2022: {'table_id': '85318NED'},
-        #     2021: {'table_id': '85039NED'}
-        # }
-
         self.table_column_ids = {
-            2024: {'table_id': '85984NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_29', 'Eenpersoonshuishoudens_30', 'HuishoudensZonderKinderen_31', 'HuishoudensMetKinderen_32', 'GemiddeldeHuishoudensgrootte_33', 'Bevolkingsdichtheid_34', 'GemiddeldeWOZWaardeVanWoningen_39', 'Koopwoningen_47', 'HuurwoningenTotaal_48', 'AantalInkomensontvangers_81', 'GemiddeldInkomenPerInkomensontvanger_82', 'GemiddeldInkomenPerInwoner_83', 'HuishOnderOfRondSociaalMinimum_90', 'PersonenautoSPerHuishouden_114', 'AfstandTotGroteSupermarkt_118']}, 
-            2023: {'table_id': '85618NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_29', 'Eenpersoonshuishoudens_30', 'HuishoudensZonderKinderen_31', 'HuishoudensMetKinderen_32', 'GemiddeldeHuishoudensgrootte_33', 'Bevolkingsdichtheid_34', 'GemiddeldeWOZWaardeVanWoningen_36', 'Koopwoningen_41', 'HuurwoningenTotaal_42', 'AantalInkomensontvangers_76', 'GemiddeldInkomenPerInkomensontvanger_77', 'GemiddeldInkomenPerInwoner_78', 'HuishOnderOfRondSociaalMinimum_85', 'PersonenautoSPerHuishouden_109', 'AfstandTotGroteSupermarkt_113']}, 
-            2022: {'table_id': '85318NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_28', 'Eenpersoonshuishoudens_29', 'HuishoudensZonderKinderen_30', 'HuishoudensMetKinderen_31', 'GemiddeldeHuishoudensgrootte_32', 'Bevolkingsdichtheid_33', 'GemiddeldeWOZWaardeVanWoningen_35', 'Koopwoningen_40', 'HuurwoningenTotaal_41', 'AantalInkomensontvangers_70', 'GemiddeldInkomenPerInkomensontvanger_71', 'GemiddeldInkomenPerInwoner_72', 'HuishOnderOfRondSociaalMinimum_79', 'PersonenautoSPerHuishouden_103', 'AfstandTotGroteSupermarkt_107']}, 
-            2021: {'table_id': '85039NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_28', 'Eenpersoonshuishoudens_29', 'HuishoudensZonderKinderen_30', 'HuishoudensMetKinderen_31', 'GemiddeldeHuishoudensgrootte_32', 'Bevolkingsdichtheid_33', 'GemiddeldeWOZWaardeVanWoningen_35', 'Koopwoningen_40', 'HuurwoningenTotaal_41', 'AantalInkomensontvangers_70', 'GemiddeldInkomenPerInkomensontvanger_71', 'GemiddeldInkomenPerInwoner_72', 'HuishOnderOfRondSociaalMinimum_79', 'PersonenautoSPerHuishouden_103', 'AfstandTotGroteSupermarkt_107']}
+            2024: {'table_id': '85984NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_29', 'Eenpersoonshuishoudens_30', 'HuishoudensZonderKinderen_31', 'HuishoudensMetKinderen_32', 'GemiddeldeHuishoudensgrootte_33', 'Bevolkingsdichtheid_34', 'GemiddeldeWOZWaardeVanWoningen_39', 'Koopwoningen_47', 'HuurwoningenTotaal_48', 'AantalInkomensontvangers_81', 'GemiddeldInkomenPerInkomensontvanger_82', 'GemiddeldInkomenPerInwoner_83', 'HuishOnderOfRondSociaalMinimum_90', 'PersonenautoSPerHuishouden_114', 'AfstandTotGroteSupermarkt_118', 'BuitenEuropa_19', 'BuitenEuropa_22', 'BuitenEuropa_24']}, 
+            2023: {'table_id': '85618NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_29', 'Eenpersoonshuishoudens_30', 'HuishoudensZonderKinderen_31', 'HuishoudensMetKinderen_32', 'GemiddeldeHuishoudensgrootte_33', 'Bevolkingsdichtheid_34', 'GemiddeldeWOZWaardeVanWoningen_36', 'Koopwoningen_41', 'HuurwoningenTotaal_42', 'AantalInkomensontvangers_76', 'GemiddeldInkomenPerInkomensontvanger_77', 'GemiddeldInkomenPerInwoner_78', 'HuishOnderOfRondSociaalMinimum_85', 'PersonenautoSPerHuishouden_109', 'AfstandTotGroteSupermarkt_113', 'BuitenEuropa_19', 'BuitenEuropa_22', 'BuitenEuropa_24']}, 
+            2022: {'table_id': '85318NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_28', 'Eenpersoonshuishoudens_29', 'HuishoudensZonderKinderen_30', 'HuishoudensMetKinderen_31', 'GemiddeldeHuishoudensgrootte_32', 'Bevolkingsdichtheid_33', 'GemiddeldeWOZWaardeVanWoningen_35', 'Koopwoningen_40', 'HuurwoningenTotaal_41', 'AantalInkomensontvangers_70', 'GemiddeldInkomenPerInkomensontvanger_71', 'GemiddeldInkomenPerInwoner_72', 'HuishOnderOfRondSociaalMinimum_79', 'PersonenautoSPerHuishouden_103', 'AfstandTotGroteSupermarkt_107', 'BuitenEuropa_19', 'BuitenEuropa_22', 'BuitenEuropa_24']}, 
+            2021: {'table_id': '85039NED', 'cols': ['ID', 'WijkenEnBuurten', 'Gemeentenaam_1', 'SoortRegio_2', 'AantalInwoners_5', 'k_0Tot15Jaar_8', 'k_15Tot25Jaar_9', 'k_25Tot45Jaar_10', 'k_45Tot65Jaar_11', 'k_65JaarOfOuder_12', 'HuishoudensTotaal_28', 'Eenpersoonshuishoudens_29', 'HuishoudensZonderKinderen_30', 'HuishoudensMetKinderen_31', 'GemiddeldeHuishoudensgrootte_32', 'Bevolkingsdichtheid_33', 'GemiddeldeWOZWaardeVanWoningen_35', 'Koopwoningen_40', 'HuurwoningenTotaal_41', 'AantalInkomensontvangers_70', 'GemiddeldInkomenPerInkomensontvanger_71', 'GemiddeldInkomenPerInwoner_72', 'HuishOnderOfRondSociaalMinimum_79', 'PersonenautoSPerHuishouden_103', 'AfstandTotGroteSupermarkt_107', 'BuitenEuropa_19', 'BuitenEuropa_22', 'BuitenEuropa_24']}
         }
 
         self.kenmerken = {
@@ -37,6 +31,10 @@ class CBSData:
             'Eenpersoonshuishoudens': {'nieuwe_naam': '% Eenpersoonshuishoudens', 'value_type': '%', 'formatting': '{:.0%}'}, 
             'HuishoudensZonderKinderen': {'nieuwe_naam': '% Huishoudens zonder kinderen', 'value_type': '%', 'formatting': '{:.0%}'}, 
             'HuishoudensMetKinderen': {'nieuwe_naam': '% Huishoudens met kinderen', 'value_type': '%', 'formatting': '{:.0%}'}, 
+
+            'Buiten Europa (herkomstland)': {'nieuwe_naam': 'Buiten Europa (herkomstland)', 'value_type': '%', 'formatting': '{:.0%}'},
+            'Buiten Europa (geboren in Nederland)': {'nieuwe_naam': 'Buiten Europa (geboren in Nederland)', 'value_type': '%', 'formatting': '{:.0%}'},
+            'Buiten Europa (geboren buiten Nederland)': {'nieuwe_naam': 'Buiten Europa (geboren buiten Nederland)', 'value_type': '%', 'formatting': '{:.0%}'},
             
             'k_0Tot15Jaar': {'nieuwe_naam': '% 0-14 jaar', 'value_type': '%', 'formatting': '{:.0%}'}, 
             'k_15Tot25Jaar': {'nieuwe_naam': '% 15-24 jaar', 'value_type': '%', 'formatting': '{:.0%}'}, 
@@ -44,12 +42,12 @@ class CBSData:
             'k_45Tot65Jaar': {'nieuwe_naam': '% 45-64 jaar', 'value_type': '%', 'formatting': '{:.0%}'}, 
             'k_65JaarOfOuder': {'nieuwe_naam': '% +65 jaar', 'value_type': '%', 'formatting': '{:.0%}'}, 
 
-            'GemiddeldInkomenPerInwoner': {'nieuwe_naam': 'Gemiddeld inkomen per inwoner', 'value_type': 'abs', 'formatting': '€ {:.0f}X000'}, 
+            'GemiddeldInkomenPerInwoner': {'nieuwe_naam': 'Gemiddeld inkomen per inwoner', 'value_type': 'abs', 'formatting': '€ {:,.0f}'},                      # 
             'AantalInkomensontvangers': {'nieuwe_naam': 'Aantal inkomensontvangers', 'value_type': '%', 'formatting': '{:.0%}'}, 
-            'GemiddeldInkomenPerInkomensontvanger': {'nieuwe_naam': 'Gemiddeld inkomen per inkomensontvanger', 'value_type': 'abs', 'formatting': '€ {:.0f}X000'}, 
+            'GemiddeldInkomenPerInkomensontvanger': {'nieuwe_naam': 'Gemiddeld inkomen per inkomensontvanger', 'value_type': 'abs', 'formatting': '€ {:,.0f}'},  # 
             'HuishOnderOfRondSociaalMinimum': {'nieuwe_naam': 'Huishoudens onder of rond sociaal minumum', 'value_type': 'abs', 'formatting': '{:.1f}%'},
 
-            'GemiddeldeWOZWaardeVanWoningen': {'nieuwe_naam': 'Gemiddelde WOZ-waarde', 'value_type': 'abs', 'formatting': '€ {:.0f}X000'}, 
+            'GemiddeldeWOZWaardeVanWoningen': {'nieuwe_naam': 'Gemiddelde WOZ-waarde', 'value_type': 'abs', 'formatting': '€ {:,.0f}'},                          # 
             'Koopwoningen': {'nieuwe_naam': '% Koopwoningen', 'value_type': 'abs', 'formatting': '{:.0f}%'}, 
             'HuurwoningenTotaal': {'nieuwe_naam': ' % Huurwoningen', 'value_type': 'abs', 'formatting': '{:.0f}%'}, 
             'PersonenautoSPerHuishouden': {'nieuwe_naam': "Gemiddeld aantal auto's per huishouden", 'value_type': 'abs', 'formatting': '{:,.2f}'}, 
@@ -65,6 +63,9 @@ class CBSData:
         # Data ophalen vanuit CBS API
         data0 = cbsodata.get_data(self.table_column_ids[jaar]['table_id'], select=self.table_column_ids[jaar]['cols'])
         df = pd.DataFrame(data0)
+
+        # 'BuitenEuropa_19', 'BuitenEuropa_22', 'BuitenEuropa_24' kolommen renamen, omdat in de volgende coderegel de suffixen worden verwijderd
+        df = df.rename(columns={'BuitenEuropa_19': 'Buiten Europa (herkomstland)', 'BuitenEuropa_22': 'Buiten Europa (geboren in Nederland)', 'BuitenEuropa_24': 'Buiten Europa (geboren buiten Nederland)'})
 
         df.columns = df.columns.str.replace(r'_\d+$', '', regex=True)   # suffix verwijderen
         df = df[self.kenmerken.keys()]                                  # Alleen benodigde kolommen selecteren
@@ -84,42 +85,80 @@ class CBSData:
         return df
 
     def calculate_afwijking(self, A, B):
-        try:
-            afwijking = (B-A)/A
-            # afwijking = f'{afwijking: .1%}'
-            return afwijking
-        except:
-            return ''
+        if pd.isna(A) or pd.isna(B):
+            return None
+        else:
+            try:
+                afwijking = (B-A)/A
+                # afwijking = f'{afwijking: .1%}'
+                return afwijking
+            except:
+                return None
+
+    # def format_cells(self, df):
+    #     # NL en regio kolommen formatten
+    #     for k, v in self.kenmerken.items():
+    #         formatting_pattern = v['formatting']
+
+    #         if formatting_pattern not in {'', None}:
+    #             for col in df.columns[:2]:
+    #                 try:
+    #                     df.at[k, col] = formatting_pattern.format(df.at[k, col]).replace(',', 'X').replace('.', ',').replace('X', '.')
+    #                 except:
+    #                     pass
         
+    #     # Afwijking kolom formatten
+    #     formatted_afwijkingen = []
+        
+    #     for afwijking in df['Afwijking']:
+    #         try:
+    #             formatted_afwijking = '{:.1%}'.format(afwijking).replace(',', 'X').replace('.', ',').replace('X', '.')
+    #             formatted_afwijkingen.append(formatted_afwijking)
+    #         except:
+    #             formatted_afwijkingen.append(afwijking)
+
+    #     df['Afwijking'] = formatted_afwijkingen
+
+    #     # Kenmerken formatten
+    #     df.insert(0, 'Kenmerk', df.index)
+    #     df = df.reset_index(drop=True)
+    #     df['Kenmerk'] = df['Kenmerk'].replace({k:v['nieuwe_naam'] for k,v in self.kenmerken.items()})
+    #     df = df.style.set_properties(subset=['Kenmerk'], **{'text-align': 'left'})
+
+    #     return df
+
     def format_cells(self, df):
-        # NL en regio kolommen formatten
-        for k, v in self.kenmerken.items():
+        # Kolommen Nederland en [regio]
+        for k, v in self.kenmerken.items(): # data -> self
             formatting_pattern = v['formatting']
 
-            if formatting_pattern not in {'', None}:
-                for col in df.columns[:2]:
-                    try:
-                        df.at[k, col] = formatting_pattern.format(df.at[k, col]).replace(',', 'X').replace('.', ',').replace('X', '.')
-                    except:
-                        pass
-        
-        # Afwijking kolom formatten
-        formatted_afwijkingen = []
-        
-        for afwijking in df['Afwijking']:
-            try:
-                formatted_afwijking = '{:.1%}'.format(afwijking).replace(',', 'X').replace('.', ',').replace('X', '.')
-                formatted_afwijkingen.append(formatted_afwijking)
-            except:
-                formatted_afwijkingen.append(afwijking)
+            if formatting_pattern != '': 
 
+                for col in df.columns[:2]:
+                    cell_value = df.at[k, col]
+
+                    if pd.isna(cell_value) == False:
+
+                        if k in ['GemiddeldInkomenPerInwoner', 'GemiddeldInkomenPerInkomensontvanger', 'GemiddeldeWOZWaardeVanWoningen']:
+                            cell_value = cell_value * 1000
+
+                        df.at[k, col] = formatting_pattern.format(cell_value).replace(',', 'X').replace('.', ',').replace('X', '.')
+
+        # Kolom afwijking
+        formatted_afwijkingen = []
+        for afwijking in df['Afwijking']:
+            if pd.isna(afwijking) == False:
+                afwijking = '{:.1%}'.format(afwijking).replace(',', 'X').replace('.', ',').replace('X', '.')
+            
+            formatted_afwijkingen.append(afwijking)
+        
         df['Afwijking'] = formatted_afwijkingen
 
         # Kenmerken formatten
         df.insert(0, 'Kenmerk', df.index)
         df = df.reset_index(drop=True)
         df['Kenmerk'] = df['Kenmerk'].replace({k:v['nieuwe_naam'] for k,v in self.kenmerken.items()})
-        df = df.style.set_properties(subset=['Kenmerk'], **{'text-align': 'left'})
+        # df = df.style.set_properties(subset=['Kenmerk'], **{'text-align': 'left'})
 
         return df
 
@@ -149,42 +188,6 @@ class CBSData:
             self.df_regio_formatted = df_formatted
 
 ############################################################################
-
-# st.title('Bevolkingskarakteristiek')
-# img = st.image('https://gmo-research.ai/en/application/files/5816/6011/7522/GettyImages-1150668297_1.jpg')
-
-# with st.sidebar:
-#     jaar = st.pills('Jaar', [2024, 2023, 2022, 2021], default=2024, selection_mode="single")
-
-# # API
-# api_button = st.button('Verbinden met CBS API')
-# if api_button:
-#     with st.spinner('Data ophalen vanuit CBS API (dit kan een paar minuten duren...)'):
-#         data = CBSData(jaar=jaar)
-
-#     st.success('Data opgehaald!', icon="✅")
-#     st.session_state['data'] = data
-
-# # Data verwerken
-# if 'data' in st.session_state:
-#     data = st.session_state['data']
-
-#     with st.sidebar:
-#         dropdown_values = [f'{id} - {WeB} ({gemeentenaam})' for id, WeB, gemeentenaam in zip(data.df['ID'], data.df['WijkenEnBuurten'], data.df['Gemeentenaam'])]
-
-#         selection = st.selectbox(
-#             "Regio",
-#             dropdown_values,  # dropdown menu
-#             placeholder="Selecteer een regio..."
-#         )
-#         selection_id = int(re.search(r'\d+', selection).group())
-
-#     if st.button('Run', type='primary', icon='🏃‍♂️‍➡️'):
-#         data.filter_regio(idx=selection_id, formatted=True)
-
-#         img.empty()
-
-#         st.dataframe(data.df_regio_formatted, height=1000, use_container_width=True, hide_index=True)
 
 st.title('Bevolkingskarakteristiek')
 img = st.image('https://gmo-research.ai/en/application/files/5816/6011/7522/GettyImages-1150668297_1.jpg')
@@ -224,7 +227,7 @@ if 'meerjarendata' in st.session_state:
         )
         selection_id = int(re.search(r'\d+', selection).group())
 
-    if st.button('Run', type='primary', icon='🏃‍♂️‍➡️'):
+    if st.button("Los geht's", type='primary', icon='🔥'):
 
         # Dynamically create tabs based on the list
         tabs = st.tabs([str(year) for year in meerjarendata.keys()])
@@ -235,7 +238,26 @@ if 'meerjarendata' in st.session_state:
                 st.header(f"{year}")
 
                 meerjarendata[year].filter_regio(idx=selection_id, formatted=True)
-                st.dataframe(meerjarendata[year].df_regio_formatted, height=1000, use_container_width=True, hide_index=True)
+                # st.dataframe(meerjarendata[year].df_regio_formatted, height=1000, use_container_width=True, hide_index=True)
+
+
+
+
+
+                # Split dataframes:
+
+                # Define index ranges
+                ranges = [(0, 4), (4, 11), (11, 14), (14, 19), (19, 23), (23, 27)]  # (start, stop)
+
+                # Split DataFrame
+                split_dfs = []
+                for start, stop in ranges:
+                    mini_df = meerjarendata[year].df_regio_formatted.iloc[start:stop]
+                    mini_df = mini_df.style.set_properties(subset=['Kenmerk'], **{'text-align': 'left'})
+                    split_dfs.append(mini_df)
+
+                for sd in split_dfs:
+                    st.dataframe(sd, use_container_width=True, hide_index=True)
                 
 
 # streamlit run /Users/quincyliem/My\ Drive/Projects/Kardol/Bevolkingskarakteristiek/bevolkingskarakterstiek2.py
